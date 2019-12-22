e8631f80f062d6d2b76827fb1aa74a3536512377
marco-chen4u <haisheng.chen4u@foxmail.com>
2019-12-21 20:51:17 -0600

    create the project to make it runnable

---
+++ src/main/java/com/marco/rabbitmq/RabbitmqdemoApplication.java
@@ -0,0 +1,23 @@
+package com.marco.rabbitmq;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqdemoApplication implements CommandLineRunner {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqdemoApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        rabbitTemplate.convertAndSend("Hi there! this the 3rd greeting from RabbitMQ!");
+    }
+}
